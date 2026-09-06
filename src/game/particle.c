5449d48106a5b5dffe945dee45e84cacabaff6f5
lenny <268398021+LennyFang@users.noreply.github.com>
2026-09-05 23:08:59 -0400

    Add in randomness to init velo and acceleration to particles

--- src/game/particle.c
+++ src/game/particle.c
@@ -1,10 +1,17 @@
 #define MAX_I32 2147483647
 
+// generates a random f32 from 0.0 to 1.0
 internal f32
 rand_f32()
 {
     i32 value = GetRandomValue(0, MAX_I32);
     return (f32)value / (f32)MAX_I32;
+}
+
+internal f32
+rand_f32_range(f32 min, f32 max)
+{
+    return min + (rand_f32() * (max - min));
 }
 
 Emitter 
@@ -19,14 +26,14 @@ init_emitter(Arena* backing_arena, f32 life_time, f32 spawn_rate, u32 spawn_amou
     // Allocations
     u64 cap = (sizeof(Vector2)+sizeof(Vector2)+sizeof(f32))*emitter.max_particles;
     void* backing_mem = arena_allocate(backing_arena, cap);
-    printf("backing mem %p\n", backing_mem);
+    DebugLog("backing mem %p", backing_mem);
     emitter.arena = arena_init(backing_mem, cap);
-    emitter.pos = (Vector2*)arena_allocate(&emitter.arena, sizeof(Vector2)*emitter.max_particles);
-    printf("backing pos %p\n", emitter.pos);
-    emitter.vel = (Vector2*)arena_allocate(&emitter.arena, sizeof(Vector2)*emitter.max_particles);
-    printf("backing vel %p\n", emitter.vel);
-    emitter.time = (f32*)arena_allocate(&emitter.arena, sizeof(f32)*emitter.max_particles);
-    printf("backing time %p\n", emitter.time);
+    emitter.particle_pos = (Vector2*)arena_allocate(&emitter.arena, sizeof(Vector2)*emitter.max_particles);
+    DebugLog("backing particle_pos %p", emitter.particle_pos);
+    emitter.particle_vel = (Vector2*)arena_allocate(&emitter.arena, sizeof(Vector2)*emitter.max_particles);
+    DebugLog("backing particle_vel %p", emitter.particle_vel);
+    emitter.particle_time = (f32*)arena_allocate(&emitter.arena, sizeof(f32)*emitter.max_particles);
+    DebugLog("backing particle_time %p", emitter.particle_time);
 
     return emitter;
 }
@@ -43,17 +50,17 @@ void uar_emitter(Emitter* emitter, f32 delta)
     {
         for (i32 i=0; i<emitter->spawn_amount; i++)
         {
-            printf("can you work please\n");
+            DebugLog("can you work please");
             // NOTE: There is a timing issues with the removing in spawning
             if (emitter->current_particles >= emitter->max_particles)
             {
-                printf("In use\n");
+                DebugLog("In use");
                 break;
             }
-            // TODO: Remove the temp variables for pos and vel
-            emitter->pos[emitter->current_particles] = (Vector2){100, 100};
-            emitter->vel[emitter->current_particles] = (Vector2){100, 100};
-            emitter->time[emitter->current_particles] = 0.0f;
+            // TODO: Change pos to spawn depending on the shape rn it is just point
+            emitter->particle_pos[emitter->current_particles] = emitter->pos;
+            emitter->particle_vel[emitter->current_particles] = (Vector2){rand_f32_range(emitter->init_vel_min.x, emitter->init_vel_max.x), rand_f32_range(emitter->init_vel_min.y, emitter->init_vel_max.y)};
+            emitter->particle_time[emitter->current_particles] = 0.0f;
             emitter->current_particles += 1;
 
             // NOTE: Placed this here in case of the early break
@@ -66,25 +73,30 @@ void uar_emitter(Emitter* emitter, f32 delta)
 
     for (i32 i=0; i<emitter->current_particles;)
     {
-        printf("time %f\n", emitter->time[i]);
         // Check for dead particles
-        if (emitter->time[i] >= 1.0)
+        if (emitter->particle_time[i] >= 1.0)
         {
-            printf("\n1 dead %d %d\n", emitter->current_particles, i);
-            emitter->pos[i] = emitter->pos[emitter->current_particles-1];
-            emitter->vel[i] = emitter->vel[emitter->current_particles-1];
-            emitter->time[i] = emitter->time[emitter->current_particles-1];
+            DebugLog("1 dead %d %d", emitter->current_particles, i);
+            emitter->particle_pos[i] = emitter->particle_pos[emitter->current_particles-1];
+            emitter->particle_vel[i] = emitter->particle_vel[emitter->current_particles-1];
+            emitter->particle_time[i] = emitter->particle_time[emitter->current_particles-1];
             emitter->current_particles -= 1;
-            printf("\n2 dead %d %d\n", emitter->current_particles, i);
+            DebugLog("2 dead %d %d", emitter->current_particles, i);
             continue;
         }
+
         // Update alive particles
-        emitter->pos[i].x += emitter->vel[i].x * delta;
-        emitter->pos[i].y += emitter->vel[i].y * delta;
-        emitter->time[i] += delta / emitter->life_time;
-        printf("id %d pos %f %f\n", i, emitter->pos[i].x, emitter->pos[i].y);
+        emitter->particle_vel[i].x += emitter->accel.x * delta;
+        emitter->particle_vel[i].y += emitter->accel.y * delta;
+
+        emitter->particle_pos[i].x += emitter->particle_vel[i].x * delta;
+        emitter->particle_pos[i].y += emitter->particle_vel[i].y * delta;
+
+        DebugLog("id %d particle_pos %f %f", i, emitter->particle_pos[i].x, emitter->particle_pos[i].y);
         // Render particles
-        DrawCircleV(emitter->pos[i], 10, RED);
+        DrawCircleV(emitter->particle_pos[i], 4, RED);
+
+        emitter->particle_time[i] += delta / emitter->life_time;
         ++i;
     }
 }
--- src/game/particle.h
+++ src/game/particle.h
@@ -1,9 +1,19 @@
+typedef enum Emitter_Shape Emitter_Shape;
+enum Emitter_Shape
+{
+    Shape_Point,
+    Shape_Rectangle,
+    Shape_Circle,
+    Shape_Count
+};
+
 typedef struct Emitter Emitter;
-struct Emitter {
+struct Emitter
+{
     Arena arena;
-    Vector2* pos;
-    Vector2* vel;
-    f32* time;
+    Vector2* particle_pos;
+    Vector2* particle_vel;
+    f32* particle_time;
 
     // Set Params
     f32 spawn_rate;
@@ -11,9 +21,29 @@ struct Emitter {
     f32 life_time; // in seconds
     u32 max_particles;
 
+    // Spawner Shape
+    Emitter_Shape shape;
+    Vector2 pos;
+
     // Runtime Trackers
     f32 timer;
     u32 current_particles;
+
+    // Acceleration
+    Vector2 accel;
+
+    // Init Velocity
+    Vector2 init_vel_min;
+    Vector2 init_vel_max;
+
+    // Size
+    f32 init_size_min;
+    f32 init_size_max;
+    // f32 final_size;
+
+    // Colour TODO: yeah idk how to lerp these into a gradient lol
+    Color init_colour;
+    Color final_colour;
 };
 
 Emitter init_emitter(Arena* backing_arena, f32 life_time, f32 spawn_rate, u32 spawn_amount);
